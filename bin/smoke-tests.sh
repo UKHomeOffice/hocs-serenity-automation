283eaa382621496604f3bfc8e020bdb361caa66b
Dom Barnett <dom.barnett@ten10.com>
2018-09-05 15:31:56 +0100

    Offline dependency use

--- bin/smoke-tests.sh
+++ bin/smoke-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B "-Dcucumber.options=--tags @critical" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -Dwebdriver.remote.driver=chrome
+mvn clean verify -B -o "-Dcucumber.options=--tags @critical" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -Dwebdriver.remote.driver=chrome
