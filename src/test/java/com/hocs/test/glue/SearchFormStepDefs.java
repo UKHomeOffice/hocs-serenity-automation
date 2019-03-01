eb0f9950923652eefd3136123376bea796c1b47b
Eamon <droko_eamon@hotmail.co.uk>
2019-03-01 16:12:38 +0000

    searchFormFeature

---
+++ src/test/java/com/hocs/test/glue/SearchFormStepDefs.java
@@ -0,0 +1,15 @@
+package com.hocs.test.glue;
+
+import com.hocs.test.pages.Page;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+
+public class SearchFormStepDefs {
+
+    @Managed
+    WebDriver driver;
+
+    Page page;
+
+}
