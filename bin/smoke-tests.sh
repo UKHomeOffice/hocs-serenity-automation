74f90d2911ba9191e6e3372345de193bbc331d4c
Cassey Prosser <casey.prosser@ten10.com>
2020-11-06 16:02:06 +0000

    test commit

--- bin/smoke-tests.sh
+++ bin/smoke-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean verify "-Dcucumber.options=--tags '@DCURegression or @MPAMRegression or @Regression'" -Dchrome.switches=--headless -Denvironment=QA
+mvn clean verify "-Dcucumber.options=--tags '@DCURegression or @MPAMRegression or @Regression'"
