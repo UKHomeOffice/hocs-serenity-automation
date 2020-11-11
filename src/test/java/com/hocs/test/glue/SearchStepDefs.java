fd043e8714027617e9af33e1c5cc5322e4806f71
Cassey Prosser <casey.prosser@ten10.com>
2020-11-11 12:57:50 +0000

    added page element for 'MTS Case' radio button added 'MTS' case type to creation and check stage steps added 'MTS' example to createCase documents scenario created 'Newly create MTS cases should move to the Data Input stage' scenario

--- src/test/java/com/hocs/test/glue/SearchStepDefs.java
+++ src/test/java/com/hocs/test/glue/SearchStepDefs.java
@@ -228,7 +228,7 @@ public class SearchStepDefs extends BasePage {
         search.assertCurrentCaseIsDisplayedInSearchResults();
     }
 
-    @And("I search for an MPAM case with {string} as it's {string}")
+    @And("I search for an UKVI case with {string} as it's {string}")
     public void searchForMPAMCaseWith(String infoValue, String infoType) {
         if (search.mpamCaseCheckbox.isVisible()) {
             safeClickOn(search.mpamCaseCheckbox);
@@ -238,7 +238,7 @@ public class SearchStepDefs extends BasePage {
         safeClickOn(searchButton);
     }
 
-    @And("I check that the MPAM search results have the correct {string}")
+    @And("I check that the UKVI search results have the correct {string}")
     public void checkMPAMCaseHasCorrect(String infoType) {
         WebElementFacade topSearchResult = findBy("//tr[1]/td/a[contains(text(), 'MPAM') or contains(text(), 'MTS')]");
         setSessionVariable("topSearchResult").to(topSearchResult.getText());
