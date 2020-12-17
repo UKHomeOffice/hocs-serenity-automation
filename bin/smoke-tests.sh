d90b8055c1e6beb938c0f91c7f4f50394a861319
Cassey Prosser <casey.prosser@ten10.com>
2020-12-17 15:29:59 +0000

    commit for testing run in drone

--- bin/smoke-tests.sh
+++ bin/smoke-tests.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B "-Dcucumber.options=--tags  @test" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -Dwebdriver
-.remote.driver=chrome -e
+mvn clean verify -B "-Dcucumber.options=--tags  @test" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e
