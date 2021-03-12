9667bfe2bada72d9868507945d89bbd7321151ac
Cassey Prosser <casey.prosser@ten10.com>
2021-03-12 13:16:12 +0000

    added environment variables for QAX

--- src/test/java/config/Environment.java
+++ src/test/java/config/Environment.java
@@ -3,10 +3,12 @@ package config;
 public enum Environment {
 
     LOCAL("http://localhost"),
+    QA("https://qa.internal.cs-notprod.homeoffice.gov.uk/"),
+    QAX("https://qax.internal.cs-notprod.homeoffice.gov.uk/"),
     DEV("https://dev.internal.cs-notprod.homeoffice.gov.uk/"),
     DEMO("https://demo.cs-notprod.homeoffice.gov.uk/"),
-    QA("https://qa.internal.cs-notprod.homeoffice.gov.uk/"),
     MANAGEMENTUIQA("https://qa-management.internal.cs-notprod.homeoffice.gov.uk"),
+    MANAGEMENTUIQAX("https://qax-management.internal.cs-notprod.homeoffice.gov.uk/"),
     MANAGEMENTUIDEV("http://dev-management.internal.cs-notprod.homeoffice.gov.uk/"),
     MANAGEMENTUIDEMO("http://demo-management.cs-notprod.homeoffice.gov.uk/");
 
