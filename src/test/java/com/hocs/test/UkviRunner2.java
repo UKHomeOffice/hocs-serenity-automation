1d4c6ff48cc5a690c31a914452f5b51b98c05d84
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-04-21 16:02:19 +0100

    Tags on Runner

---
+++ src/test/java/com/hocs/test/UkviRunner2.java
@@ -0,0 +1,12 @@
+package com.hocs.test;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/ukvi2",tags="@UKVIRegression")
+public class UkviRunner2 {
+
+}
