98719282b2f183604e5e09c0bdca9ada204d2a87
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-04-22 10:10:05 +0100

    Full Regression Parallel Execution on GRID

--- src/test/java/com/hocs/test/DcuRunner.java
+++ src/test/java/com/hocs/test/DcuRunner.java
@@ -6,7 +6,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/",tags="@test")
+@CucumberOptions(features = "src/test/resources/features/",tags="@DCURegression")
 public class DcuRunner {
 
 }
