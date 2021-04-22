409a84d076aaf30d2ab44ea20dae4e0442db0380
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-04-22 10:21:12 +0100

    Full Regression

--- bin/smoke-tests.sh
+++ bin/smoke-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@test" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e
+mvn clean verify -B -Dcucumber.filter.tags="@DCURegression or @UKVIRegression or @Regression " -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e
