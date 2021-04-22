d9b9d54a148fe68d54aa18ddeb63dda8bf9fe5a9
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-04-22 12:54:11 +0100

    Normal run @test1-4

--- src/test/java/com/hocs/test/DcuRunner.java
+++ src/test/java/com/hocs/test/DcuRunner.java
@@ -6,7 +6,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/dcu")
+@CucumberOptions(features = "src/test/resources/features")
 public class DcuRunner {
 
 }
