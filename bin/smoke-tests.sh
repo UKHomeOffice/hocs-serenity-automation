209b026abae3b2f32a8213c3a358d2c86698772c
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-04-22 17:27:13 +0100

    Zalenium Test 6

--- bin/smoke-tests.sh
+++ bin/smoke-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@test1 or @test2 or @test3 or @test4 " -Dwebdriver.remote.url=http://zalenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome
+mvn clean verify -B -Dcucumber.filter.tags="@test1" -Dwebdriver.remote.url=http://zalenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome
