9e11585e9deb3025390d63d035ec32d93ff781b2
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-04-23 12:57:45 +0100

    Parallel Test 20

--- bin/smoke-tests2.sh
+++ bin/smoke-tests2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@test2" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome
--Dserenity.outputDirectory="xtarget/site/serenity/"
+mvn clean verify -B -Dcucumber.filter.tags="@test2" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote
+.driver=chrome -Dserenity.outputDirectory="xtarget"
