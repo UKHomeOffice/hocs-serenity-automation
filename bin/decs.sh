fb576067ffe69219ea3cd5c12512b6ef3668a71d
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-05-05 13:13:56 +0100

    QAX

--- bin/decs.sh
+++ bin/decs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@Regression" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome
+mvn clean verify -B -Dcucumber.filter.tags="@Regression" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome -Denvironment=QAX
