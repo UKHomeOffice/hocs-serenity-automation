b278212b99f5939cc9c28e8535d312cf41d562ff
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-05-06 10:02:29 +0100

    DELTA test 1

--- bin/decs.sh
+++ bin/decs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@Regression" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome -Denvironment=DELTA
+mvn clean verify -B -Dcucumber.filter.tags="@test" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome
+-Denvironment=DELTA
