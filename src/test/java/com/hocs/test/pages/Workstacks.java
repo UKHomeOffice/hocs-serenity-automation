9a3964037af548c00857f30125c049d1882ab4a1
srikanthgona <gsrikanth.k@gmail.com>
2021-05-13 19:03:56 +0100

    Added wait for getStageFromWorkstacksTable()

--- src/test/java/com/hocs/test/pages/Workstacks.java
+++ src/test/java/com/hocs/test/pages/Workstacks.java
@@ -457,7 +457,7 @@ public class Workstacks extends BasePage {
     }
 
     private String getStageFromWorkstacksTable() {
-        workstackFilter.waitUntilVisible();
+        workstackFilter.withTimeoutOf(Duration.ofSeconds(60)).waitUntilVisible();
         WebElement caseReferenceStage = getDriver().findElement(
                 By.xpath("//a[text()='" + sessionVariableCalled("caseReference")
                         + "']/../following-sibling::td[1]"));
