32c6f3dd7d1927db13a5262a061caa4926e904c1
cameron.page <cameron.page@ten10.com>
2021-06-15 16:58:53 +0100

    Created service QA scenarios

--- src/test/java/com/hocs/test/pages/comp/ServiceQA.java
+++ src/test/java/com/hocs/test/pages/comp/ServiceQA.java
@@ -3,6 +3,7 @@ package com.hocs.test.pages.comp;
 import com.hocs.test.pages.BasePage;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
+import static net.serenitybdd.core.Serenity.setSessionVariable;
 
 public class ServiceQA extends BasePage {
 
@@ -12,6 +13,9 @@ public class ServiceQA extends BasePage {
     @FindBy(xpath = "//label[text()='Reject - return response to draft']")
     public WebElementFacade returnResponseToDraftRadioButton;
 
+    @FindBy(id = "CaseNote_QaReject")
+    public WebElementFacade rejectionReasonTextField;
+
     public void moveCaseFromServiceQAToServiceSend() {
         safeClickOn(sendResponseToComplainantRadioButton);
         safeClickOn(continueButton);
@@ -20,5 +24,8 @@ public class ServiceQA extends BasePage {
     public void moveCaseFromServiceQAToServiceDraft() {
         safeClickOn(returnResponseToDraftRadioButton);
         safeClickOn(continueButton);
+        rejectionReasonTextField.sendKeys("Test Rejection Reason");
+        setSessionVariable("rejectionReason").to("Test Rejection Reason");
+        clickTheButton("Reject");
     }
 }
--- src/test/resources/features/comp/ServiceQA.feature
+++ src/test/resources/features/comp/ServiceQA.feature
@@ -9,13 +9,14 @@ Feature: Service QA
 #    HOCS-3695
   @COMPWorkflow @COMPRegression
   Scenario: User can accept the response and send the case to Service Send stage
-    When I accept the response
+    When I "accept" the response at the Service QA stage
     Then the case should be moved to the "Service Send" stage
     And the summary should display the owning team as "CCT Stage 1 Response Team"
 
 #    HOCS-3039
   @COMPWorkflow @COMPRegression
   Scenario: User can reject the response and send the case back to Service Draft stage
-    When I reject the response
+    When I "reject" the response at the Service QA stage
     Then the case should be moved to the "Service Draft" stage
     And the summary should display the owning team as "CCT Stage 1 Response Team"
+    And a rejection note should be visible showing the reason for rejection
