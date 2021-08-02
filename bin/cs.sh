4952917fc9a0da70a6d91679cba6a98844bf32be
Kiran <Kiran.Manne@homeoffice.gov.uk>
2021-08-02 17:07:03 +0100

    points to cs-qa

--- bin/cs.sh
+++ bin/cs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@CSRegression" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome -Denvirionment=qax
+mvn clean verify -B -Dcucumber.filter.tags="@CSRegression" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote.driver=chrome -Denvirionment=cs-qa
