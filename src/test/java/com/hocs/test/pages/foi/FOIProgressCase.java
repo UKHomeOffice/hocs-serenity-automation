ea22126e0068359bbf4bbe99eab66156ae67bb8f
cameron.page <cameron.page@ten10.com>
2021-08-10 15:07:15 +0100

    Updated progress case to store case data and changed foi user to foi regression user

--- src/test/java/com/hocs/test/pages/foi/FOIProgressCase.java
+++ src/test/java/com/hocs/test/pages/foi/FOIProgressCase.java
@@ -2,8 +2,11 @@ package com.hocs.test.pages.foi;
 
 import com.hocs.test.pages.decs.BasePage;
 import com.hocs.test.pages.decs.Documents;
+import com.hocs.test.pages.decs.RecordCaseData;
 
 public class FOIProgressCase extends BasePage {
+
+    RecordCaseData recordCaseData;
 
     FOIApproval foiApproval;
 
@@ -11,37 +14,37 @@ public class FOIProgressCase extends BasePage {
 
     public void moveCaseFromCaseCreationToKIMUAllocation() {
         clickTheButton("Confirm");
-        selectSpecificRadioButtonFromGroupWithHeading("Yes", "Is this a valid request?");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("Yes", "Is this a valid request?");
         safeClickOn(continueButton);
         waitABit(250);
         safeClickOn(continueButton);
     }
 
     public void moveCaseFromKIMUAllocationToAcceptance() {
-        selectRandomRadioButtonFromGroupWithHeading("Type of request");
+        recordCaseData.selectRandomRadioButtonFromGroupWithHeading("Type of request");
         safeClickOn(continueButton);
-        selectRandomOptionFromDropdownWithHeading("Directorate");
-        selectRandomOptionFromDropdownWithHeading("Acceptance Team");
-        enterSpecificTextIntoTextAreaWithHeading("Test support transfer comment", "Comment to support transfer");
+        recordCaseData.selectRandomOptionFromDropdownWithHeading("Directorate");
+        recordCaseData.selectRandomOptionFromDropdownWithHeading("Acceptance Team");
+        recordCaseData.enterSpecificTextIntoTextAreaWithHeading("Test support transfer comment", "Comment to support transfer");
         clickTheButton("Finish");
     }
 
     public void moveCaseFromAcceptanceToConsiderAndDraft() {
-        selectSpecificRadioButtonFromGroupWithHeading("Yes", "Does this case belong to your Directorate?");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("Yes", "Does this case belong to your Directorate?");
         clickTheButton("Continue");
-        selectRandomOptionFromDropdownWithHeading("Please select the team required for drafting the response");
+        recordCaseData.selectRandomOptionFromDropdownWithHeading("Please select the team required for drafting the response");
         clickTheButton("Finish");
     }
 
     public void moveCaseFromConsiderAndDraftToApproval() {
-        selectSpecificRadioButtonFromGroupWithHeading("Yes", "Is this case for your drafting team?");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("Yes", "Is this case for your drafting team?");
         clickTheButton("Continue");
-        selectSpecificRadioButtonFromGroupWithHeading("No", "Do you need to request contributions?");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("No", "Do you need to request contributions?");
         clickTheButton("Continue");
-        selectSpecificRadioButtonFromGroupWithHeading("Full disclosure", "What is the response type?");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("Full disclosure", "What is the response type?");
         clickTheButton("Continue");
         safeClickOn(documents.addDocumentsButton);
-        selectSpecificOptionFromDropdownWithHeading("Draft response", "Document type");
+        recordCaseData.selectSpecificOptionFromDropdownWithHeading("Draft response", "Document type");
         documents.uploadDocumentOfType("docx");
         clickTheButton("Add");
         clickTheButton("Continue");
@@ -49,23 +52,23 @@ public class FOIProgressCase extends BasePage {
 
     public void moveCaseFromApprovalToDispatch() {
         safeClickOn(foiApproval.addAnApprovalRequestHypertext);
-        selectRandomOptionFromDropdownWithHeading("Approver Role");
-        enterDateIntoDateFieldsWithHeading(getTodaysDate(), "Approval request date");
-        enterDateIntoDateFieldsWithHeading(getDatePlusMinusNDaysAgo(10), "Approval due date");
+        recordCaseData.selectRandomOptionFromDropdownWithHeading("Approver Role");
+        recordCaseData.enterDateIntoDateFieldsWithHeading(getTodaysDate(), "Approval request date");
+        recordCaseData.enterDateIntoDateFieldsWithHeading(getDatePlusMinusNDaysAgo(10), "Approval due date");
         clickTheButton("Add");
         safeClickOn(foiApproval.editHypertext);
-        selectSpecificRadioButtonFromGroupWithHeading("Complete", "Approval Status");
-        selectRandomRadioButtonFromGroupWithHeading("Decision");
-        enterDateIntoDateFieldsWithHeading(getTodaysDate(), "Approval received date");
-        enterSpecificTextIntoTextFieldWithHeading("Test Approver", "Approver Name");
-        enterSpecificTextIntoTextAreaWithHeading("Test Details Text", "Details");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("Complete", "Approval Status");
+        recordCaseData.selectRandomRadioButtonFromGroupWithHeading("Decision");
+        recordCaseData.enterDateIntoDateFieldsWithHeading(getTodaysDate(), "Approval received date");
+        recordCaseData.enterSpecificTextIntoTextFieldWithHeading("Test Approver", "Approver Name");
+        recordCaseData.enterSpecificTextIntoTextAreaWithHeading("Test Details Text", "Details");
         clickTheButton("Update");
         waitABit(250);
         clickTheButton("Continue");
     }
 
     public void moveCaseFromDispatchToSoftClose() {
-        selectSpecificRadioButtonFromGroupWithHeading("Yes", "Are you sure you want to dispatch this case?");
+        recordCaseData.selectSpecificRadioButtonFromGroupWithHeading("Yes", "Are you sure you want to dispatch this case?");
         clickTheButton("Continue");
     }
 }
