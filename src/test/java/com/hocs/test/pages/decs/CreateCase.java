8c189b9f8fa5f6db2b6c01a5a5463d7bbda728a7
srikanthgona <gsrikanth.k@gmail.com>
2021-10-20 09:33:34 +0100

    added condition for HOCS-3933

--- src/test/java/com/hocs/test/pages/decs/CreateCase.java
+++ src/test/java/com/hocs/test/pages/decs/CreateCase.java
@@ -188,7 +188,10 @@ public class CreateCase extends BasePage {
         }
         selectCaseType(caseType);
         safeClickOn(nextButton);
-//        documents.uploadDocumentOfType("docx");
+        //TODO : Remove below if statement once the defect HOCS-3933 is closed
+        if(caseType!="SMC") {
+            documents.uploadDocumentOfType("docx");
+        }
         storeCorrespondenceReceivedDate();
         clickCreateCaseButton();
         createCaseSuccessPage.storeCaseReference();
