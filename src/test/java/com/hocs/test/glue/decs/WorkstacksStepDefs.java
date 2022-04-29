06ab0543c203fefaf51cd36765517d2d544c78ab
srikanthgones <gsrikanth.k@gmail.com>
2022-04-29 14:46:15 +0100

    removed unused stepdefs

--- src/test/java/com/hocs/test/glue/decs/WorkstacksStepDefs.java
+++ src/test/java/com/hocs/test/glue/decs/WorkstacksStepDefs.java
@@ -138,11 +138,6 @@ public class WorkstacksStepDefs extends BasePage {
         workstacks.goToCurrentCaseFromWorkstack();
     }
 
-    @And("I click the link for the transferred case in the workstack")
-    public void iClickTheLinkForTheTransferredCase() {
-        workstacks.selectSpecificCaseReferenceLink(sessionVariableCalled("newCaseReference"));
-    }
-
     @And("I filter the workstack using the current cases reference")
     public void iFilterTheWorkstackUsingTheCurrentCasesReference() {
         workstacks.filterByCurrentCaseReference();
