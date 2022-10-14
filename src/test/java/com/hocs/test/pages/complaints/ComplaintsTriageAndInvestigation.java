50e0bc08b2084f8dc78742a58eefbe17defab8fd
srikanthgones <gsrikanth.k@gmail.com>
2022-10-14 16:36:25 +0100

    fix for HOCS_5632

--- src/test/java/com/hocs/test/pages/complaints/ComplaintsTriageAndInvestigation.java
+++ src/test/java/com/hocs/test/pages/complaints/ComplaintsTriageAndInvestigation.java
@@ -160,8 +160,10 @@ public class ComplaintsTriageAndInvestigation extends BasePage {
     }
 
     public void selectIEDETClaimCategory(String category) {
+        setSessionVariable("complaintCategory").to(category);
         List<WebElementFacade> claimCategories = findAll("//span[text()='" + category + "']/parent::legend/following-sibling::div//label");
-        recordCaseData.checkRandomCheckboxFromList(claimCategories);
+        String selectedClaimCategory = checkRandomCheckboxFromList(claimCategories);
+        setSessionVariable("claimCategory").to(selectedClaimCategory);
     }
 
     public void selectAVisibleClaimCategory() {
@@ -392,4 +394,6 @@ public class ComplaintsTriageAndInvestigation extends BasePage {
         String enteredReason = recordCaseData.enterTextIntoTextAreaWithHeading("Reason for transfer");
         setSessionVariable("transferReason").to(enteredReason);
     }
+
+
 }
