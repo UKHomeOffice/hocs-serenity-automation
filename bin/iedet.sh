df7e6610c0952795cd97fb56361a4e0a96976331
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2023-03-27 14:30:14 +0100

    Remove unused Selenium hub, we only support one browser. the browser we support isn't Chrome but this is a step forward.

--- bin/iedet.sh
+++ bin/iedet.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-mvn clean verify -B -Dcucumber.filter.tags="@IEDETRegression" -Dwebdriver.remote.url=http://selenium:4444/wd/hub -e -Dwebdriver.remote
-.driver=chrome
+mvn clean verify -B -Dcucumber.filter.tags="@IEDETRegression" -e
