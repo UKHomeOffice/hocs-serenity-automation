f20e7ac4f663b3084565e9d93906bdb2fc9236c6
MadhumitaSuresh2i <114481327+MadhumitaSuresh2i@users.noreply.github.com>
2023-04-06 09:29:00 +0100

    HOCS-6199 DCU changes (#822)

--- src/test/java/com/hocs/test/pages/dcu/MinisterialSignOff.java
+++ src/test/java/com/hocs/test/pages/dcu/MinisterialSignOff.java
@@ -32,7 +32,7 @@ public class MinisterialSignOff extends BasePage {
     }
 
     public void selectNotApplicableToApproveResponse() {
-        recordCaseData.selectSpecificRadioButton(" No - return to Private Office Approval");
+        recordCaseData.selectSpecificRadioButton("No - return to Private Office Approval");
     }
 
     public void enterRejectionReason() {
